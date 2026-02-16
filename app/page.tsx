d8dfcc1ad1053aea304c6a19dd20fa891d2d887d
Rhys Evans <rhys.evans@ft.com>
2026-02-16 21:22:52 +0000

    show/hide utility buttons

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,85 @@ import { useSurvey, useSurveyDispatch } from "@/app/components/SurveyProvider";
 import Link from "next/link";
 import { exportToExcel } from "@/app/lib/excel-export";
 
+function Toolbar({
+  showTools,
+  setShowTools,
+  isFrozen,
+  toggleFreeze,
+  exportToExcel,
+}: {
+  showTools: boolean;
+  setShowTools: (showTools: boolean) => void;
+  isFrozen: boolean;
+  toggleFreeze: () => void;
+  exportToExcel: (
+    type: "ebird" | "lvrpa" | "stats",
+    format?: "csv" | "xlsx",
+  ) => void;
+}) {
+  return (
+    <>
+      <div
+        className={`flex items-center justify-start gap-2 flex-wrap fixed top-0 left-0 right-0 bg-base-100 p-2 transition-transform duration-300 transform z-9  ${showTools ? "translate-y-0" : "-translate-y-full"}`}
+      >
+        {isFrozen ? (
+          <>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => exportToExcel("lvrpa")}
+            >
+              Export LVRPA
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => exportToExcel("stats")}
+            >
+              Export stats
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => exportToExcel("ebird", "csv")}
+            >
+              Export Ebird
+            </button>
+            <Link className="btn btn-secondary" href="/table-view">
+              Table View
+            </Link>
+          </>
+        ) : null}
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={toggleFreeze}
+        >
+          {isFrozen ? "Unfreeze survey" : "Freeze survey"}
+        </button>
+
+        <Link className="btn btn-primary" href="/new-survey">
+          Start New Survey
+        </Link>
+      </div>
+      <button
+        type="button"
+        className="btn fixed top-0 right-0 m-2 z-10 btn-circle"
+        onClick={() => setShowTools(!showTools)}
+      >
+        {showTools ? (
+          <span className="icon-[tabler--chevron-up] icon-size-4"></span>
+        ) : (
+          <span className="icon-[tabler--chevron-down] icon-size-4"></span>
+        )}
+      </button>
+    </>
+  );
+}
+
 export default function Home() {
   const [activeTab, setActiveTab] = useState(sectorsList[0].id);
+  const [showTools, setShowTools] = useState(false);
   const dispatch = useSurveyDispatch();
   const activeTabRef = useRef<HTMLButtonElement>(null);
   const surveyData = useSurvey();
@@ -45,8 +122,17 @@ export default function Home() {
   }
   return (
     <main>
+      <Toolbar
+        showTools={showTools}
+        setShowTools={setShowTools}
+        isFrozen={surveyData.isFrozen}
+        toggleFreeze={toggleFreeze}
+        exportToExcel={(type, format) =>
+          exportToExcel(surveyData, type, format)
+        }
+      />
       <nav
-        className="tabs tabs-bordered overflow-x-auto gap-2 fixed top-0 left-0 right-0 z-10 bg-base-100"
+        className="tabs tabs-bordered overflow-x-auto gap-2 fixed top-0 left-0 right-0 z-5 bg-base-100"
         aria-label="Tabs"
         role="tablist"
         aria-orientation="horizontal"
@@ -70,7 +156,7 @@ export default function Home() {
         ))}
       </nav>
 
-      <div className="mt-15 p-2">
+      <div className="mt-15 p-2 mb-7">
         <div
           id={`tab-content`}
           role="tabpanel"
@@ -85,48 +171,6 @@ export default function Home() {
             title={`${activeIndex + 1}: ${activeSector.name}`}
           />
         </div>
-      </div>
-
-      <div className="flex items-center justify-start gap-2 flex-wrap">
-        {surveyData.isFrozen ? (
-          <>
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => exportToExcel(surveyData, "lvrpa")}
-            >
-              Export LVRPA
-            </button>
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => exportToExcel(surveyData, "stats")}
-            >
-              Export stats
-            </button>
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => exportToExcel(surveyData, "ebird", "csv")}
-            >
-              Export Ebird
-            </button>
-            <Link className="btn btn-secondary" href="/table-view">
-              Table View
-            </Link>
-          </>
-        ) : null}
-        <button
-          type="button"
-          className="btn btn-secondary"
-          onClick={toggleFreeze}
-        >
-          {surveyData.isFrozen ? "Unfreeze survey" : "Freeze survey"}
-        </button>
-
-        <Link className="btn btn-primary" href="/new-survey">
-          Start New Survey
-        </Link>
       </div>
     </main>
   );
