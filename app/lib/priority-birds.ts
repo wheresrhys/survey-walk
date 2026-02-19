8fcc0a8af32ce9102bc19e8f827a9f183887c67f
Rhys Evans <rhys.evans@ft.com>
2026-02-19 20:01:02 +0000

    add more prioritisation filters

--- app/lib/priority-birds.ts
+++ app/lib/priority-birds.ts
@@ -13,33 +13,43 @@ function lvrpaNameToShortName(name: string): string | undefined {
 }
 
 function getTopBirds(sector: string, month: string, dayOfMonth: number): Set<string> {
+  const distilledHistoryEntries = Object.entries(distilledHistory);
   const nextMonth = String((Number(month) % 12) + 1).padStart(2, "0");
   const prevMonth = String(((Number(month) + 10) % 12) + 1).padStart(2, "0");
-  const dataToFilter = Object.entries(distilledHistory);
+
   const topBirds = new Set<string>();
   const filters: SpeciesFilter[] = [
     // Birds seen in every year in this sector;
-    ([, monthFrequencies]) =>
-      monthFrequencies[month]?.[sector] === monthFrequency[month],
+    ([, distributionForBird]) =>
+      distributionForBird[month]?.[sector] === monthFrequency[month],
     // Birds seen in most years in this sector
-    ([, monthFrequencies]) =>
-      monthFrequencies[month]?.[sector] >= monthFrequency[month] - 1,
+    ([, distributionForBird]) =>
+      distributionForBird[month]?.[sector] >= monthFrequency[month] - 1,
     // Birds seen every year in next month in this sector
-    ([, monthFrequencies]) =>
-      dayOfMonth >=12 && monthFrequencies[nextMonth]?.[sector] === monthFrequency[nextMonth],
+    ([, distributionForBird]) =>
+      dayOfMonth >=12 && distributionForBird[nextMonth]?.[sector] === monthFrequency[nextMonth],
     // Birds seen every year in prev month in this sector
-    ([, monthFrequencies]) =>
-      dayOfMonth <= 20 && monthFrequencies[prevMonth]?.[sector] === monthFrequency[prevMonth],
+    ([, distributionForBird]) =>
+      dayOfMonth <= 20 && distributionForBird[prevMonth]?.[sector] === monthFrequency[prevMonth],
     // Birds seen most years in next month in this sector
-    ([, monthFrequencies]) =>
-      dayOfMonth >= 20 && monthFrequencies[nextMonth]?.[sector] >= monthFrequency[nextMonth] -1 ,
+    ([, distributionForBird]) =>
+      dayOfMonth >= 20 && distributionForBird[nextMonth]?.[sector] >= monthFrequency[nextMonth] -1 ,
     // Birds seen most years in prev month in this sector
-    ([, monthFrequencies]) =>
-      dayOfMonth <= 10 && monthFrequencies[prevMonth]?.[sector] >= monthFrequency[prevMonth]- 1,
+    ([, distributionForBird]) =>
+      dayOfMonth <= 10 && distributionForBird[prevMonth]?.[sector] >= monthFrequency[prevMonth]- 1,
+    // Birds seen in at least 2 years in this sector
+    ([, distributionForBird]) =>
+      distributionForBird[month]?.[sector] >= 2,
+    // Birds seen at least one in this sector
+    ([, distributionForBird]) =>
+      distributionForBird[month]?.[sector] >= 1,
+    // Birds seen in every month _somewhere_
+    ([, distributionForBird]) =>
+      Object.keys(distributionForBird).length === 12,
   ];
   let i = 0;
   while (topBirds.size < 24 && i < filters.length) {
-    dataToFilter.filter(filters[i]).forEach(([species]) => {
+    distilledHistoryEntries.filter(filters[i]).forEach(([species]) => {
       const shortName = lvrpaNameToShortName(species);
       if (shortName) {
         topBirds.add(shortName);
